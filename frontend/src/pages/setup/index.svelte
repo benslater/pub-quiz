121c4f547e0b6b93f5e2a911ac546b85dca8aa6c
Ben Slater <ben.slater5@gmail.com>
2020-04-26 11:27:06 +0100

    Rework setup page FE

--- frontend/src/pages/setup/index.svelte
+++ frontend/src/pages/setup/index.svelte
@@ -5,18 +5,21 @@
   export let currentRoute;
   export let params;
 
-  let questions = [{}];
+  let rounds = [[{}]];
 
-  const addQuestion = () => {
-    questions = [...questions, {}];
-    console.log(questions);
+  const addQuestion = roundIndex => {
+    rounds[roundIndex] = [...rounds[roundIndex], {}];
+  };
+
+  const addRound = () => {
+    rounds = [...rounds, [{}]];
   };
 
   const createGame = async () => {
     const res = await fetch("http://localhost:1337/game/create", {
       method: "POST",
       body: JSON.stringify({
-        questions
+        rounds
       })
     });
     const { id } = await res.json();
@@ -32,32 +35,40 @@
 
 <div>
   <h2>Enter questions and answers</h2>
-  {#each questions as question, index}
-    <div class="row">
-      <div class="col sm-1">{index + 1})</div>
-      <div class="col sm-5">
-        <div class="form-group">
-          <label for={`question-input-${index + 1}`}>Question:</label>
-          <input
-            bind:value={questions[index].question}
-            class="input-block"
-            type="text"
-            id={`question-input-${index + 1}`} />
+  {#each rounds as questions, roundIndex}
+    <div class="card">
+      <h3>Round {roundIndex + 1}</h3>
+      {#each questions as question, questionIndex}
+        <div class="row">
+          <div class="col sm-1">{questionIndex + 1})</div>
+          <div class="col sm-5">
+            <div class="form-group">
+              <label for={`question-input-${questionIndex + 1}`}>
+                Question:
+              </label>
+              <input
+                bind:value={questions[questionIndex].question}
+                class="input-block"
+                type="text"
+                id={`question-input-${questionIndex + 1}`} />
+            </div>
+          </div>
+          <div class="col sm-5">
+            <div class="form-group">
+              <label for={`answer-input-${questionIndex + 1}`}>Answer:</label>
+              <input
+                bind:value={questions[questionIndex].answer}
+                class="input-block"
+                type="text"
+                id={`answer-input-${questionIndex + 1}`} />
+            </div>
+          </div>
+          <button class="col sm-1">x</button>
         </div>
-      </div>
-      <div class="col sm-5">
-        <div class="form-group">
-          <label for={`answer-input-${index + 1}`}>Answer:</label>
-          <input
-            bind:value={questions[index].answer}
-            class="input-block"
-            type="text"
-            id={`answer-input-${index + 1}`} />
-        </div>
-      </div>
-      <div class="col sm-1">DELETE</div>
+      {/each}
+      <button on:click={() => addQuestion(roundIndex)}>Add question</button>
     </div>
   {/each}
-  <button on:click={addQuestion}>Add question</button>
+  <button on:click={addRound}>Add round</button>
   <button on:click={createGame}>Create game</button>
 </div>
